86c49687ab65ee93ea27fa9cb778e4ad435dc102
No1Austin <amadiaustin@rocketmail.com>
2026-04-08 02:02:41 -0400

    backend-reposition-commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const Order = require("./models/order");
+const Order = require("./backend/models/order");
 
 dotenv.config();
 
