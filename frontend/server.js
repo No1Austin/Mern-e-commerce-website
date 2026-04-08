4374fa039db40a254b2ad9ea642ad9039b8ae946
No1Austin <amadiaustin@rocketmail.com>
2026-04-08 07:59:16 -0400

    webpatch1

--- frontend/server.js
+++ frontend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const Order = require("./backend/models/order");
+const Order = require("../backend/models/Order");
 
 dotenv.config();
 
